cd42228085362ba3a1ce6bbb23fbe4abebbb41b2
Damian Montero <photoca@hotmail.com>
2020-10-24 17:21:30 -0400

    Adding slide 1

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 // my variables
 let middleThird = document.getElementById('middleThird');
+let input = document.createElement("INPUT"); 
 let lowerThird = document.getElementById('lowerThird');
 
 
-
-let addButton=(word,css)=>{
-      /* Create an button element */ 
-var word1 = document.createElement("button"); 
-              
-/* Set the type class */ 
-word1.setAttribute("class", css);
-
-word1.setAttribute('name',word)
-/* Append node to the body */ 
-lowerThird.appendChild(word1); 
 
 let addButton = (word, css) => {
     /* Create an button element */
@@ -25,43 +15,47 @@ let addButton = (word, css) => {
     word1.innerHTML==word
 
     word1.setAttribute('id', word)
-    document.getElementById(word).innerHTML='word'
+    console.log(word1)
+    console.log(word)
 
     /* Append node to the body */
     lowerThird.appendChild(word1);
 }
+let populateButton=(word)=>{
+    document.getElementById(word).innerHTML=word;
 
-let inputFill = (placeholder, css, type) => {
-    /* Create an input element */
-    let placeholder1 = document.createElement("INPUT");
+}
 
-    /* Set the type attribute */
-    placeholder1.setAttribute("type", type);
+let inputFill = (placeholder,css,type)=>{
+/* Create an input element */ 
+let placeholder1 = document.createElement("INPUT"); 
+              
+/* Set the type attribute */ 
+placeholder1.setAttribute("type", type); 
+  
+/* Set the value to the PLACEHOLDER */ 
+placeholder1.setAttribute("placeholder", placeholder); 
+  
+/* Set the value to the class */ 
+placeholder1.setAttribute("class", css); 
 
-    /* Set the value to the PLACEHOLDER */
-    placeholder1.setAttribute("placeholder", placeholder);
-
-    /* Set the value to the class */
-    placeholder1.setAttribute("class", css);
-
-    /* Append node to the body */
-    middleThird.appendChild(placeholder1);
-
-
+/* Append node to the body */ 
+middleThird.appendChild(placeholder1);
 
 }
 
 
-let removeAllChildNodes = (parent) => {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
-}
+let removeAllChildNodes=(parent)=> {
+      while (parent.firstChild) {
+          parent.removeChild(parent.firstChild);
+      }
+  }
 
 
-let Slide1 = () => {
-    removeAllChildNodes(middleThird);
-    inputFill('First Name');
-    inputFill('Last Name');
-
-}
+let slide1 = () =>{
+removeAllChildNodes(middleThird);
+inputFill('First Name',null,'text');
+inputFill('Last Name',null,'text');
+addButton('next','smallBar')
+populateButton('next')
+ }
